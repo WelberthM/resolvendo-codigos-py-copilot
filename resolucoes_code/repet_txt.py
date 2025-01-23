1c6a6b68339b0856878090402607e1aa85f6c14b
AlmTech <welberthmorais@outlook.com>
2025-01-23 05:35:08 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,14 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+# Solicitando a entrada dos números
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+# Realizando a operação
+
+resultado = num1 + num2;
+
+# Imprimindo o resultado
+
+print("O resultado da soma é: ", resultado);
+
