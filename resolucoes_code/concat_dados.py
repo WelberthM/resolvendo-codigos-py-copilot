2118ed135cec2cef5cc3804d6b78abfe70bde30e
AlmTech <welberthmorais@outlook.com>
2025-01-23 05:52:59 -0300

    Finalizando

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,9 @@
-# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+# 1 - Solicitando a entrada dos dados
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# 2 - Concatenando os dados com um espaço entre eles
+resultado = dado1 + " " + dado2
+
+# 3 - Exibindo o resultado final
+print("O resultado da concatenação é:", resultado)
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,14 +1,11 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
 
-# Solicitando a entrada dos números
-num1 = int(input('Digite o primeiro número: '))
-num2 = int(input('Digite o segundo número: '))
+# Solicitando a entrada dos dados
+string = input("Digite algo: ")
+num2 = int(input("Digite o número de repetições: "))
 
-# Realizando a operação
-
-resultado = num1 + num2;
+# Realizando a operação (repetindo a string)
+resultado = string * num2
 
 # Imprimindo o resultado
-
-print("O resultado da soma é: ", resultado);
-
+print("O resultado da multiplicação da string é:", resultado)
